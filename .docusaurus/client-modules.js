a23141dc27d92a6cf810f084591a8cb7240dd3e2
aidamiscom <aidamiscom@protonmail.com>
2025-01-03 10:27:30 -0500

    Update gh-pages deployment

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/usr/local/src/mainstreamtoken.com123/node_modules/infima/dist/css/default/default.css"),
+  require("/usr/local/src/mainstreamtoken.com123/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/usr/local/src/mainstreamtoken.com123/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/usr/local/src/mainstreamtoken.com123/src/css/custom.css"),
+];
