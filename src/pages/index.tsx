aa4ced77df37e6d5d9b11770b8fae395d0d1c773
aidamiscom <aidamiscom@protonmail.com>
2025-01-05 16:10:54 -0500

    feat: Add additional paragraph to homepage header description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -271,6 +271,8 @@ function HomepageHeader() {
                     <p style={{ fontSize: '1.2rem', color: 'var(--ifm-font-color-base)' }}>
                         An experimental protocol for digital content licensing with token holdings.
                         This system gives alternative options for content creators to define a parametric licensing framework*.
+				</p>
+                    <p style={{ fontSize: '1.2rem', color: 'var(--ifm-font-color-base)' }}>
                         We envision a world where creators define their own terms, turning digital rights into mainstream tokens.
                     </p>
                     <div className={styles.buttons}>
